8f2fbf9ab3d6acbcc779e6e627f2810d6a6dd36f
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-09-25 18:03:55 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const PORT = 8080;
+
+const database = {
+    users: [
+        {
+            id: '101',
+            name: 'Ramesh',
+            email: 'ramesh@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date(),
+        },
+        {
+            id: '102',
+            name: 'Suresh',
+            email: 'suresh@gmail.com',
+            password: 'cakes',
+            entries: 0,
+            joined: new Date(),
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users)
+});
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email
+        && req.body.password === database.users[0].password) {
+        res.json('success')
+    } else {
+        res.status(400).json('error logging in');
+    }
+});
+
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    database.users.push({
+        id: '103',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date(),
+    });
+    res.json(database.users[database.users.length - 1])
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user)
+        }
+    });
+    if (!found) {
+        res.status(400).json('no such user')
+    }
+})
+
+app.post('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries)
+        }
+    });
+    if (!found) {
+        res.status(400).json('no such user')
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`app is running on PORT : ${PORT}.`);
+})
+
